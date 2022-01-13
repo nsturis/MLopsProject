63c107d4856b35d6e87d22b5788afea9ff7e6ef0
Jungar111 <asger@tang.dk>
2022-01-13 10:37:43 +0100

    linting

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'It's raining cats and dogs'
+project = u"It's raining cats and dogs"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
