56259525a4d297d348384a1a232f08c9b30366e9
Laurine <dargaud.laurine@gmail.com>
2022-01-21 10:56:31 +0100

    change testing

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -1,17 +1,18 @@
 import os
 import hydra
 from hydra.utils import get_original_cwd
-from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import WandbLogger
 from src.data.dataloader import AnimalDataModule
 from src.config import DOGCATConfig
-from model import Classifier
 from google.cloud import secretmanager
+import torch
+import wandb
+import torchmetrics
+import numpy as np
 
 
 @hydra.main(config_path="../conf", config_name="config")
 def main(cfg: DOGCATConfig):
-    print("Predicting like a boss")
+    print("Testing up to heaven")
 
     PROJECT_ID = "onyx-glider-337908"
     secrets = secretmanager.SecretManagerServiceClient()
@@ -20,14 +21,10 @@ def main(cfg: DOGCATConfig):
     ).payload.data.decode("utf-8")
     os.environ["WANDB_API_KEY"] = WANDB_KEY
 
-    wandb_logger = WandbLogger(project="MLOps", entity="mlops_flajn", name="Testing")
-    wandb_logger.experiment.config.update(cfg)
+    wandb.init(config=cfg, project="MLOps", entity="mlops_flajn", name="Testing")
 
     # get trained model
-    model = Classifier.load_from_checkpoint(
-        checkpoint_path=get_original_cwd() + "/models/initial_model.ckpt",
-        **{"cfg": cfg}
-    )
+    model = torch.jit.load(get_original_cwd() + '/models/model_bigboy.pt')
 
     # get test dataloader
     data_module = AnimalDataModule(
@@ -39,9 +36,26 @@ def main(cfg: DOGCATConfig):
     test_loader = data_module.test_dataloader()
 
     # testing
-    trainer = Trainer(logger=wandb_logger)
-    trainer.test(model, dataloaders=test_loader)
+    all_acc, all_loss = [],[]
+    i = 0
+    for X, y in iter(test_loader):
+        print(f"Step {i+1}")
+        logits = model(X)
+        cross_entropy_loss = torch.nn.CrossEntropyLoss()
+        loss = cross_entropy_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        test_acc = torchmetrics.Accuracy()
+        acc = test_acc(preds, y)
+        all_acc.append(acc)
+        all_loss.append(loss)
+        i+=1
 
+    all_acc, all_loss = torch.tensor(np.array(all_acc)), torch.tensor(np.array(all_loss))
+
+    print("Test_loss = ", all_loss.mean())
+    print("Test_acc = ", all_acc.mean())
+    wandb.log({"test_accuracy": all_acc.mean()})
+    wandb.log({"test_loss": all_loss.mean()})
 
 if __name__ == "__main__":
     main()
